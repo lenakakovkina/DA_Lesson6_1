ed24b59a0124bb12b9b9df386cd32c52fc085c00
Samsung <elenakakovkina89@gmail.com>
2024-07-03 22:20:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int goodsQuantity = 100;
+int goodsLoadedInCar = 0;
+
+while (goodsQuantity > 0)
+{
+    Console.WriteLine("Please enter the quantity of good loaded into a car");
+    goodsLoadedInCar = int.Parse(Console.ReadLine());
+
+    if (goodsLoadedInCar > goodsQuantity || goodsLoadedInCar <= 0)
+    {
+        Console.WriteLine("Be more careful. You enterence is incorrect!");
+        continue;
+    }
+    goodsQuantity -= goodsLoadedInCar;
+}
+Console.WriteLine("There is no more goods");
